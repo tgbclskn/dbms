132b6c018dd292b6416643517f53ef7ec5995ac5
cancanakci <102185167+cancanakci@users.noreply.github.com>
2023-12-23 01:00:37 +0300

    Initialize stlye.css
    And some baby-steps stylization for index.php

--- index.php
+++ index.php
@@ -23,20 +23,25 @@ if(!isset($_SESSION['first_visit']))
 
 
 <!DOCTYPE html>
-
-<h2>-- Lancr --</h2>
-
-<form method="post">
-	
-  <label for="uname">User Name:</label><br>
-  <input type="text" id="uname" name="username"><br>
-  
-  <label for="passwd">Password:</label><br>
-  <input type="password" id="passwd" name="password"><br><br>
-  
-  <input type="submit" formaction="login.php" value="Log In"><br>
-  <input type="submit" formaction="register.php" value="Register"><br>
-</form>
+<head>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container glassbox">
+		<h2>lancr.</h2>
+		<form method="post">
+			
+		<label for="uname">User Name:</label><br>
+		<input type="text" id="uname" name="username"><br>
+		
+		<label for="passwd">Password:</label><br>
+		<input type="password" id="passwd" name="password"><br><br>
+		
+		<input type="submit" formaction="login.php" value="Log In"><br>
+		<input type="submit" formaction="register.php" value="Register"><br>
+		</form>
+	</div>
+</body>
 
 
 
