0fb72f9e30b03c491054df6544cceb64a54d1b5d
tgbclskn <154612346+tgbclskn@users.noreply.github.com>
2023-12-23 23:05:27 +0000

    Update gigs.php

--- gigs.php
+++ gigs.php
@@ -1,26 +1,42 @@
 <?php
 	session_start();
 	
-	echo '<a href=gigenter.php>Add gig</a><br>';
+	echo '<a href=gigenter.php>Add gig&nbsp; </a>';
+	echo '<a href=mainpage.php>Mainpage</a>';
 	echo '<h3>-- My gigs --</h3><br><br>';
 	
 	
-	/* List all gigs */
+	/* List gigs belonging to user */
 	$db = new SQLite3('db.sqlite', SQLITE3_OPEN_READONLY);
 	$queryhandler = $db->query('
-					SELECT G.description,G.price,G.id FROM Gig G,User U 
-					WHERE U.id == G.ownerid');
+					SELECT G.description,
+						   G.price,
+						   G.id,
+						   C.name as categoryname
+					FROM Gigs G,Users U,Categories C 
+					WHERE U.id = G.ownerid AND 
+						  G.categoryid = C.id AND 
+						  U.name = "' . $_SESSION['user'] . '"');
 	
 	$i = 1;
 	while(true)
 	{
 		$nextgig = $queryhandler->fetchArray(SQLITE3_ASSOC);
-		if(!isset($nextgig['description']))
+		
+		if($nextgig == false)
 			break;
-		echo $i . '-<br>&emsp;Desc: ' . $nextgig['description'] 
-		. '<br>&emsp;Price: ' . $nextgig['price'] . '<br>
+		
+		echo $i . 
+		'-<br>&emsp;Desc: ' . 
+					$nextgig['description'] 
+		. '<br>&emsp;Category: ' . 
+					$nextgig['categoryname'] 
+		. '<br>&emsp;Price: ' . 
+					$nextgig['price']
+		. '<br>
+		
 		<a href="gigrem.php?id=' . $nextgig['id'] 
-		. '">Remove gig</a><br><br>';
+		. '">Remove</a><br><br>';
 		$i = $i + 1;
 	}
 	 $db->close();
